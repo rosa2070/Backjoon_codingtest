414776ac567cbc882d2b05f40e23442cb860ae49
nowsilver <46918839+rosa2070@users.noreply.github.com>
2024-09-15 02:17:17 +0900

    [Bronze V] Title: 오늘 날짜, Time: 216 ms, Memory: 18744 KB -BaekjoonHub

---
+++ 백준/Bronze/10699. 오늘 날짜/오늘 날짜.java
@@ -0,0 +1,15 @@
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
+public class Main {
+    public static void main(String[] args) {
+        Date date = new Date();
+        DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+
+        df.setTimeZone(TimeZone.getTimeZone("Asia/Seoul"));
+        System.out.println(df.format(date));
+
+    }
+}
