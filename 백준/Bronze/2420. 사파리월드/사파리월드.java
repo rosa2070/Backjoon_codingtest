c391ef3aa906c43c7101b41e889728970b62d8cc
nowsilver <46918839+rosa2070@users.noreply.github.com>
2024-09-21 23:19:28 +0900

    [Bronze V] Title: 사파리월드, Time: 164 ms, Memory: 17688 KB -BaekjoonHub

---
+++ 백준/Bronze/2420. 사파리월드/사파리월드.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long x = sc.nextInt();
+        long y = sc.nextInt();
+
+        System.out.println(Math.abs(x - y));
+
+
+
+
+
+    }
+}
